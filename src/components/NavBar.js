700a8a846f7185122327a3e3efcdc950d5b658c8
Kelly McGuffog <kellymcguffog@gmail.com>
2022-11-11 17:06:11 -0500

    rearranged navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import HboWhiteLogo from "../Assets/HBO_Max_White.png"
+import HuluLogoWhite from "../Assets/Hulu_Logo_White.png"
+import NetflixLogoWhite from "../Assets/Netflix_Logo_White.png"
+import Search from "./Search";
 
-function NavBar() {
+
+function NavBar({search, setSearch}) {
   return(
     <div className="navbar">
-        <h4><NavLink exact to="/">Home</NavLink></h4>
-        <h4><NavLink to="/hbo">HBO</NavLink></h4>
-        <h4><NavLink to="/hulu">Hulu</NavLink></h4>
-        <h4><NavLink to="/netflix">Netflix</NavLink></h4>
-        <h4><NavLink to="/new">Add New</NavLink></h4>
+        {/* <h4 className="home-button" ><NavLink id="home" exact to="/">Home</NavLink></h4> */}
+        <div className="nav-left">
+            <h4 className="home-button" ><NavLink id="home" exact to="/">Home</NavLink></h4>
+            <NavLink to="/hbo"><img className="nav-logo" src={HboWhiteLogo}/></NavLink>
+            <NavLink to="/hulu"><img className="nav-logo" src={HuluLogoWhite}/></NavLink>
+            <NavLink to="/netflix"><img className="nav-logo" src={NetflixLogoWhite}/></NavLink>
+        </div>
+        <div className="nav-right">
+        <Search  search={search} setSearch={setSearch}/>
+            {/* <NavLink to="/hbo"><img className="nav-logo" src={HboWhiteLogo}/></NavLink>
+            <NavLink to="/hulu"><img className="nav-logo" src={HuluLogoWhite}/></NavLink>
+            <NavLink to="/netflix"><img className="nav-logo" src={NetflixLogoWhite}/></NavLink> */}
+            <h4 className="add-button"><NavLink to="/new">Add New</NavLink></h4>
+        </div>
     </div>
   )
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Search({search, setSearch, handleFilter}){
+function Search({search, setSearch}){
 
     // const filterCategory = filter === "all" ? allContent : allContent.filter(content => content.category.includes(filter))
     // console.log(filterCategory)
@@ -10,12 +10,12 @@ function Search({search, setSearch, handleFilter}){
         <div className="search">
             <input placeholder="search..." onChange={(e)=> setSearch(e.target.value)} className="input-search" value={search}/>
         </div>
-        <div onChange={handleFilter} className="filterCategory">
-                Filter:
-                <input type="radio" name="category" value="All"/>All
-                <input type="radio" name="category" value="Movie"/>Movie
-                <input type="radio" name="category" value="TV Show"/>TV Show
-        </div>
+        {/* // <div onChange={handleFilter} className="filterCategory">
+        //         Filter:
+        //         <input type="radio" name="category" value="All"/>All
+        //         <input type="radio" name="category" value="Movie"/>Movie
+        //         <input type="radio" name="category" value="TV Show"/>TV Show
+        // </div> */}
         </>
       );    
 }
